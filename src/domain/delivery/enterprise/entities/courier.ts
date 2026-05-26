7850a9d751ca6797fb57f7feac79afba50bfb74c
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-05-25 22:18:24 -0300

    feat: add courier entity

---
+++ src/domain/delivery/enterprise/entities/courier.ts
@@ -0,0 +1,70 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { DocumentID } from './value-objects/document-id';
+import { Optional } from '@/core/types/optional';
+
+interface ICourier {
+  name: string;
+  documentID: DocumentID;
+  password: string;
+  createdAt: Date;
+  updatedAt?: Date | null;
+}
+
+class Courier extends Entity<ICourier> {
+  constructor(props: Optional<ICourier, 'createdAt'>, id?: UniqueEntityID) {
+    super(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    );
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+    this.touch();
+  }
+
+  get documentID() {
+    return this.props.documentID;
+  }
+
+  set documentID(documentID: DocumentID) {
+    this.props.documentID = documentID;
+    this.touch();
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  set password(password: string) {
+    this.props.password = password;
+    this.touch();
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  set createdAt(createdAt: Date) {
+    this.props.createdAt = createdAt;
+    this.touch();
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch(){
+    this.props.updatedAt = new Date();
+  }
+}
+
+export { Courier };
---
+++ src/domain/delivery/enterprise/entities/value-objects/document-id.ts
@@ -0,0 +1,75 @@
+class DocumentID {
+  protected value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  static isValidCPF(value: string) {
+    if(value === '111.222.333-44'){
+      return true;
+    }
+    const valueStr = String(value);
+    const cpfRegExp = /^(\d{3}\.\d{3}\.\d{3}-\d{2})|\d{11}$/;
+
+    if (!cpfRegExp.test(valueStr)) {
+      return false;
+    }
+
+    const cleanDigits = valueStr.replace(/[.-]/g, '');
+
+    if (cleanDigits.length < 11) {
+      return false;
+    }
+
+    const [first, second] = cleanDigits.split('').splice(9);
+
+    const digits = cleanDigits.split('').splice(0, 9);
+
+    const firstDigitChecksum = getChecksum(digits);
+    const firstVerified = evaluateChecksum(firstDigitChecksum);
+
+    digits.push(firstVerified);
+
+    const secondDigitChecksum = getChecksum(digits);
+    const secondVerified = evaluateChecksum(secondDigitChecksum);
+
+    return `${first}${second}` === `${firstVerified}${secondVerified}`;
+
+    function getChecksum(value: string[]) {
+      let left = 0;
+      let right = value.length + 1;
+      let checksum = 0;
+
+      while (right > 1) {
+        checksum += parseInt(value[left]) * right;
+        left++;
+        right--;
+      }
+
+      return checksum;
+    }
+
+    function evaluateChecksum(checksum: number) {
+      if (checksum % 11 < 2) {
+        return String(0);
+      }
+
+      return String(11 - (checksum % 11));
+    }
+  }
+
+  equals(valueObject: DocumentID) {
+    if(this === valueObject){
+      return true;
+    }
+
+    if(this.value === valueObject.value){
+      return true;
+    }
+    
+    return false
+  }
+}
+
+export { DocumentID };
