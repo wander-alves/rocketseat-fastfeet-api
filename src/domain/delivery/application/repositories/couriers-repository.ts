5aecb6451788d4334a3da2a2f2767b4f4f089b7e
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-06-08 00:10:35 -0300

    feat: add `create` and `find` methods

--- src/domain/delivery/application/repositories/couriers-repository.ts
+++ src/domain/delivery/application/repositories/couriers-repository.ts
@@ -1,9 +1,9 @@
-import { Courier } from "../../enterprise/entities/courier";
-import { DocumentID } from "../../enterprise/entities/value-objects/document-id";
+import { Courier } from '../../enterprise/entities/courier';
+import { DocumentID } from '../../enterprise/entities/value-objects/document-id';
 
 abstract class CouriersRepositiory {
-  abstract create(courier: Courier): Promise<void>
-  abstract findOneByDocumentID(documentID: DocumentID): Promise<Courier | null>
+  abstract create(courier: Courier): Promise<void>;
+  abstract findOneByDocumentID(documentID: DocumentID): Promise<Courier | null>;
 }
 
 export { CouriersRepositiory };
