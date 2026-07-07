9b14ba1d9c990125f66a8beeb7f3c584df738dd1
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-06 22:53:35 -0300

    feat: add recipient entity and repository

---
+++ tests/database/repositories/in-memory-recipients-repository.ts
@@ -0,0 +1,34 @@
+import { RecipientsRepository } from '@/domain/delivery/application/repositories/recipients-repository';
+import { Recipient } from '@/domain/delivery/enterprise/entities/recipient';
+
+class InMemoryRecipientsRepository implements RecipientsRepository {
+  public items: Recipient[] = [];
+
+  async create(recipient: Recipient) {
+    this.items.push(recipient);
+  }
+
+  async findOneById(id: string) {
+    const recipient = this.items.find((item) => item.id.value === id);
+
+    if (!recipient) {
+      return null;
+    }
+
+    return recipient;
+  }
+
+  async findOneByDocumentID(documentID: string) {
+    const recipient = this.items.find(
+      (item) => item.documentID.value === documentID,
+    );
+
+    if (!recipient) {
+      return null;
+    }
+
+    return recipient;
+  }
+}
+
+export { InMemoryRecipientsRepository };
