0a605c04f46b10704385d019c385f6dbd60a55e8
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-13 00:47:40 -0300

    feat: add shipment repository

---
+++ src/infra/database/prisma/repositories/prisma-shipments-repository.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+
+import { Shipment } from '@/domain/delivery/enterprise/entities/shipment';
+import { ShipmentsRepository } from '@/domain/delivery/application/repositories/shipments-repository';
+
+import { PrismaService } from '@/infra/database/prisma.service';
+import { PrismaShipmentMapper } from '@/infra/database/prisma/mappers/prisma-shipment-mapper';
+
+@Injectable()
+class PrismaShipmentsRepository implements ShipmentsRepository {
+  private prismaService: PrismaService;
+
+  constructor(prismaService: PrismaService) {
+    this.prismaService = prismaService;
+  }
+
+  async findOneById(id: string) {
+    const data = await this.prismaService.shipment.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!data) {
+      return null;
+    }
+
+    const shipment = PrismaShipmentMapper.toDomain(data);
+
+    return shipment;
+  }
+
+  async create(shipment: Shipment) {
+    const data = PrismaShipmentMapper.toPrisma(shipment);
+
+    await this.prismaService.shipment.create({
+      data,
+    });
+  }
+}
+
+export { PrismaShipmentsRepository };
