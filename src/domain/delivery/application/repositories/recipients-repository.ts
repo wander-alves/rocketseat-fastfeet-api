aa66bb6f42e2c391e12bce6945a16570cac639b8
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-17 22:44:30 -0300

    feat: add `delete-courier` use case

--- src/domain/delivery/application/repositories/recipients-repository.ts
+++ src/domain/delivery/application/repositories/recipients-repository.ts
@@ -4,6 +4,7 @@ abstract class RecipientsRepository {
   abstract create(recipient: Recipient): Promise<void>;
   abstract findOneById(id: string): Promise<Recipient | null>;
   abstract findOneByDocumentID(documentID: string): Promise<Recipient | null>;
+  abstract deleteOneById(id: string): Promise<void>;
 }
 
 export { RecipientsRepository };
